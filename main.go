d97f13af02deaa282cf1086ffd413abe035e5841
Amr Eid <90511895+Amreid011@users.noreply.github.com>
2025-04-12 16:09:33 +0200

    Update main.go

--- main.go
+++ main.go
@@ -62,29 +62,44 @@
 
 
 
-
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("DEBUG: Starting application...")
+	// إنشاء writer وتفريغ الإخراج
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	
+	// كتابة رسالة debug على stdout
+	fmt.Fprintln(writer, "DEBUG: Starting application...")
 
+	// كتابة نفس الرسالة على stderr أيضاً للتأكد
+	fmt.Fprintln(os.Stderr, "DEBUG: Starting application...")
+
+	// محاولة قراءة الملف
 	data, err := ioutil.ReadFile("/root/flag.txt")
 	if err != nil {
-		fmt.Println("DEBUG: Error reading /root/flag.txt:", err)
+		fmt.Fprintln(writer, "DEBUG: Error reading /root/flag.txt:", err)
+		fmt.Fprintln(os.Stderr, "DEBUG: Error reading /root/flag.txt:", err)
 		return
 	}
 
+	// التأكد من محتويات الملف
 	if len(data) == 0 {
-		fmt.Println("DEBUG: /root/flag.txt is empty!")
+		fmt.Fprintln(writer, "DEBUG: /root/flag.txt is empty!")
+		fmt.Fprintln(os.Stderr, "DEBUG: /root/flag.txt is empty!")
 	} else {
-		fmt.Println("FLAG:", string(data))
+		fmt.Fprintln(writer, "FLAG:", string(data))
+		fmt.Fprintln(os.Stderr, "FLAG:", string(data))
 	}
 }
+
 
 
 
